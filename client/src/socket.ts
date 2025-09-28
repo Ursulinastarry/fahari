b1c680493fb2c7de958146b3d3cd33dbe9ed507d
Ursulinastarry <paulinawambua8@gmail.com>
2025-09-28 13:00:35 +0300

    socket

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,7 @@
 // src/socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io("https://fahari-production.up.railway.app", {
   withCredentials: true,
   transports: ["websocket"], // force WebSocket
 });
