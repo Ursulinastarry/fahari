49e41a892a7c73fe3731ee20e6f6fb1678379cc3
ursulinastarry <paulinawambua8@gmail.com>
2025-11-24 18:08:46 +0300

    ...

--- client/src/config/baseUrl.ts
+++ client/src/config/baseUrl.ts
@@ -1,4 +1,4 @@
 // src/config/baseUrl.ts
-export const baseUrl: string = (import.meta as any).env?.VITE_API_BASE_URL || (import.meta as any).env?.REACT_APP_BASE_URL;
+export const baseUrl: string = (import.meta as any).env?.VITE_API_BASE_URL;
 
 export default baseUrl;
