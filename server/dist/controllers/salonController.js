765a10cf2000119c75f5fc193ac96353f699d14c
ursulinastarry <paulinawambua8@gmail.com>
2026-01-22 23:50:43 +0300

    dirname?

--- server/dist/controllers/salonController.js
+++ server/dist/controllers/salonController.js
@@ -5,6 +5,9 @@ import path from 'path';
 import fs from 'fs';
 import { getSalonsService } from "../services/aiService.js";
 import { createAndSendNotification } from "../services/notificationService.js";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const prisma = new PrismaClient();
 export const createSalon = async (req, res) => {
     try {
